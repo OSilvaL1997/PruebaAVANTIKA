29368fcf40d349fd6d1bd6fa8c6863a2c7581cf3
KIng-Artur98 <jorgearturo9864@gmail.com>
2023-01-18 19:00:01 -0600

    Vista de Evento

---
+++ PL/Controllers/EventoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class EventoController : Controller
+    {
+        // GET: Evento
+        //[HttpGet]
+        public ActionResult GetAll()
+        {
+            ML.Evento evento = new ML.Evento();
+            evento.Eventos = new List<object>();
+            ML.Result result = BL.Evento.GetAllEvento();
+            if (result.Correct)
+            {
+                evento.Eventos = result.Objects.ToList();
+            }
+            return View(evento);
+        }
+    }
+}
